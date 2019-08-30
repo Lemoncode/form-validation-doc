8244ef223392c65f275d2ed90ba41eba3176f4e1
Braulio Diez Botella <braulio.diez@gmail.com>
2019-08-30 21:03:16 +0200

    site structure wip

---
+++ doczrc.js
@@ -0,0 +1,41 @@
+export default {
+  menu: [
+    {
+      name: "General",
+      docs: [
+        "home",
+        "getting-started",
+        "validating-schema",
+        "validating-field",
+        "validating-record",
+        "validating-form"
+      ]
+    },
+    {
+      name: "Validators",
+      menu: [
+        "builtin-validators",
+        "custom-validators-async",
+        "custom-validators-sync",
+        "third-party-validators"
+      ]
+    },
+    {
+      name: "Messages",
+      menu: ["error-message", "internalization"]
+    },
+    {
+      name: "React Final form",
+      menu: [
+        "react-final-form",
+        "sync-validations",
+        "async-validations",
+        "record-validations"
+      ]
+    },
+    {
+      name: "api",
+      menu: ["api", "validation-schema", "validation-form", "validators"]
+    }
+  ]
+};
