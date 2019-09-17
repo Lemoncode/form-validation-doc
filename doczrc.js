527a0b310eee2a6a6b3d5db55de5a09364712388
nasdan <daniel.sanchez@lemoncode.net>
2019-09-17 09:32:39 +0200

    Update base from .env

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
+
 export default {
   typescript: true,
-  base: "/fonk-doc/",
   menu: [
     {
       name: "1. General",
       menu: [
-        { name: "Fonk", order: 1 },
-        { name: "Getting Started", order: 2 },
+        { name: "Fonk", order: 1, route: "/" },
+        {
+          name: "Getting Started",
+          order: 2,
+          route: "/fonk-doc/getting-started"
+        },
         { name: "Validation Schema", order: 3 },
         { name: "Field Validation", order: 4 },
         { name: "Record Validation", order: 5 },
